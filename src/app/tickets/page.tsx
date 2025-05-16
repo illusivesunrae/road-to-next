3aadcbea071f18b1a3e7e0fc9d3343893bda133e
illusivesunrae <racmocon@gmail.com>
2025-05-16 14:08:01 -0400

    Refactored link paths as typed routes

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
 
 const TicketsPage = () => {
     return (
@@ -8,7 +9,7 @@ const TicketsPage = () => {
         {initialTickets.map((ticket) => (
           <div key={ticket.id}>
             <h3 className="text-lg">{ticket.title}</h3>
-            <Link href={`/tickets/${ticket.id}`} className="text-sm underline">View</Link>
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
           </div>
         ))}
       </div>
