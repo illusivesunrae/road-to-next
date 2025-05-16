81d1aa5f8a39b77d4a50cd021d69f1a0d9a5c803
illusivesunrae <racmocon@gmail.com>
2025-05-16 16:38:56 -0400

    Named application and added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import "./globals.css";
+import { LucideBug } from "lucide-react"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
@@ -33,7 +34,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-          <div><Link href={homePath()} className={buttonVariants({ variant: "outline"})}>Home</Link></div>
+          <div>
+            <Link href={homePath()} className={buttonVariants({ variant: "ghost"})}>
+              <h1 className="text-lg font-semibold">BugBounty</h1><LucideBug />
+            </Link>
+          </div>
           <div><Link href={ticketsPath()} className={buttonVariants({ variant: "outline"})}>Tickets</Link></div>
         </nav>
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
