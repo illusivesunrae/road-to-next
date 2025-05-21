cba15bae9a172b2c9469d4284aa6a243eb893544
illusivesunrae <racmocon@gmail.com>
2025-05-21 14:26:34 -0400

    Extracted ticket to a new component

---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCheck, LucideFileText, LucidePencil } from "lucide-react"
+
+export const TICKET_ICONS = {
+  OPEN: <LucideFileText />,
+  IN_PROGRESS: <LucidePencil />,
+  DONE: <LucideCheck />,
+}
