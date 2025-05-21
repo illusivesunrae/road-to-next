5f6f7670ddd05b4e823421b8b200654dd53b2b3c
illusivesunrae <racmocon@gmail.com>
2025-05-21 15:29:31 -0400

    Added the ability to switch between light and dark themes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,22 @@
 import { LucideBug } from "lucide-react"
 import Link from "next/link"
 import { homePath, ticketsPath } from "@/paths"
+import { ThemeSwitcher } from "./theme/theme-switcher"
 import { buttonVariants } from "./ui/button"
 
 
 const Header = () => {
     return (
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-          <div>
-            <Link href={homePath()} className={buttonVariants({ variant: "ghost"})}>
-              <h1 className="text-lg font-semibold">BugBounty</h1><LucideBug />
-            </Link>
-          </div>
-          <div><Link href={ticketsPath()} className={buttonVariants({ variant: "default"})}>Tickets</Link></div>
+            <div className="flex align-items gap-x-2">
+                <Link href={homePath()} className={buttonVariants({ variant: "ghost"})}>
+                    <h1 className="text-lg font-semibold">BugBounty</h1><LucideBug />
+                </Link>
+            </div>
+            <div className="flex align-items gap-x-2">
+                <ThemeSwitcher />
+                <Link href={ticketsPath()} className={buttonVariants({ variant: "default"})}>Tickets</Link>
+            </div>
         </nav>
     )
 }
