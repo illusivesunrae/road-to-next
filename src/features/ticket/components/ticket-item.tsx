3ff5e3802014d1f8c604646352f6b9df5345e2cb
illusivesunrae <racmocon@gmail.com>
2025-05-22 17:27:11 -0400

    Created logic to delete a ticket

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,10 +1,13 @@
+"use client"
+
 import { Ticket } from "@prisma/client"
 import clsx from "clsx"
-import { LucideLink2 } from "lucide-react"
+import { LucideLink2, LucideTrash } from "lucide-react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { ticketPath } from "@/paths"
+import { deleteTicket } from "../actions/delete-ticket"
 import { TICKET_ICONS } from "../constants"
 
 type TicketItemProps = {
@@ -18,6 +21,17 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
             <Link href={ticketPath(ticket.id)}><LucideLink2 className="h-4 w-4" /></Link>
         </Button>
     )
+
+    const handleDeleteTicket = async () => {
+        await deleteTicket(ticket.id)
+    }
+
+    const deleteButton = (
+        <Button variant="outline" size="icon" onClick={handleDeleteTicket}>
+            <LucideTrash className="h-4 w-4" />
+        </Button>
+    )
+
     return (
         <div className={clsx("w-full flex gap-x-1", {
             "max-w-[580px]": isDetail,
@@ -36,9 +50,7 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
                 })}>{ticket.content}</p>
                 </CardContent>
             </Card>
-            { isDetail ? null : (
-                <div className="flex flex-col gap-y-1">{ detailButton }</div> 
-            )}
+            <div className="flex flex-col gap-y-1">{ isDetail ? deleteButton : detailButton }</div>
         </div>
     )
 }
